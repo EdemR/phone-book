3805e943affe5a4df7dd340ef6c67ca0a9749c08
EdemR <teboldadam@gmail.com>
2021-04-29 11:26:17 +0200

    Finish code

--- src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
@@ -1,3 +1,27 @@
 package hu.nive.ujratervezes.io.phonebook;
 
-public class Phonebook {}
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+
+public class Phonebook {
+    public void exportPhonebook(Map<String, String> contacts, String output) {
+        if (contacts == null || output == null) {
+            throw new IllegalArgumentException();
+        }
+
+        File file = new File(output);
+
+        try (BufferedWriter bf = new BufferedWriter(new FileWriter(file))) {
+            for (Map.Entry<String, String> entry : contacts.entrySet()) {
+                bf.write(entry.getKey() + ": " + entry.getValue());
+                bf.newLine();
+            }
+            bf.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
